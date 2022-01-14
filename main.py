cb80de94e1aaa30f4f54e10671c79458687e3a89
Steven Bischoff <stevenbischoff2002@gmx.net>
2022-01-14 20:44:07 +0100

    disable unnecessary print statements

--- main.py
+++ main.py
@@ -13,17 +13,15 @@ if __name__ == "__main__":
         while True:
             scramble = scrambleGenerator.gen_scramble()
             uid = str(uuid1())
-            print(uid)
             print(f"\n{scramble}\n")
             time = input("Time (**:**.***) ---> ")
             min = 60 * 1000 * int(time.split(":")[0])
             sec = 1000 * int(time.split(":")[1].split(".")[0])
             msec = int(time.split(":")[1].split(".")[1])
-            print(min + sec + msec)
             with open("db","w") as f:
                 db[uid] = {}
                 db[uid]["scramble"] = scramble
                 db[uid]["time"] = min + sec + msec
                 f.write(json.dumps(db,indent=4))
     except KeyboardInterrupt:
-        print("stop")
+        print("\nstop\n")
