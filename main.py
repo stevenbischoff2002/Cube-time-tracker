2b6839ea73fcc22b9f0159fbf29a9bbe698d2367
Steven Bischoff <stevenbischoff2002@gmx.net>
2022-01-16 21:38:37 +0100

    added test files to .gitignore implemented CLI

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
+from pydoc import cli
 import scrambleGenerator
 import db_handler
+
+def cliMain(db):
+    run = True
+    while run:
+        try:
+            scramble = scrambleGenerator.gen_scramble()
+            print(f"\n{scramble}\n")
+            time = input("Time (**:**.***) ---> ")
+            if(time.lower() in ["stop","exit","quit"]):
+                print("Exiting")
+                break
+            min = 60 * 1000 * int(time.split(":")[0])
+            sec = 1000 * int(time.split(":")[1].split(".")[0])
+            msec = int(time.split(":")[1].split(".")[1])
+            db.addEntry(scramble,(min + sec + msec))
+        except ValueError:
+            print("invalid value")
+
+
+
+if(__name__ == "__main__"):
+    mode = str(input("[CLI/web] (CLI) ---> ")).lower()
+    if (mode == "cli" or len(mode) == 0):
+        mode = "cli"
+        print("cli")
+    elif (mode == "web"):
+        print("web")
+    else:
+        print("invalid")
+        exit()
+    dbFile = str(input("db Path ---> "))
+    db = db_handler.DB(dbFile)
+    if (mode == "cli"):
+        cliMain(db)
